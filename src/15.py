e174448bc222f46825203f38dec8a2c9d60dcedb
t0a5ted <flameburstunstoppable@gmail.com>
2021-12-17 23:17:56 +0800

    might as well put something there

--- src/15.py
+++ src/15.py
@@ -3,77 +3,99 @@ See: https://adventofcode.com/2021/day/15
 Basically a maze traversing algo implementation
 
 DIDNT MANAGE TO FIND AN ANSWER, IGNORE THIS FILE - IT'S GARBAGE
+
+Meanwhile, you can admire someone else's priority queue with heap implementation
 """
-from typing import *
-import sys
-
-sys.setrecursionlimit(10000000)
 
 
-def parse_input() -> List[List[int]]:
-    with open('input/test_test_15.txt', 'r') as file:
-        return [[int(c) for c in line] for line in file.read().split("\n")]
-
-def part1():
-    data: List[List[int]] = parse_input()
-
-    lowest_risk: int = traverse1(data, 0, 0, 0)
-
-    return lowest_risk
-
-def traverse1(maze: List[List[int]], x: int, y: int, risk: int, cache: Dict[Tuple[int, int], int] = None, prev: List[Tuple[int, int]] = None) -> int:
-    if cache == None:
-        cache = {} # python hates dicts and arrays as default params
-    if prev == None:
-        prev = []
-
-    min_risk: int = 100000 # random arbitrarily large number
-    risk_left, risk_right = 1000001, 10000001
-            
-    if x != 0 or y != 0:
-        risk += maze[y][x]
-    if x == len(maze[0]) - 1 and y == len(maze) - 1:
-        return risk
-
-
-    prev.append((x, y))
-
-
-    if len(maze[0]) - 1 > x and (x+1, y) not in prev:
-        if (x + 1, y) in cache.keys():
-            min_risk = min(min_risk, cache[(x+1, y)] + risk)
-        else:
-            risk_right: int = traverse1(maze, x+1, y, 0, cache, prev.copy())
-            min_risk = min(min_risk, risk_right + risk)
-        
-    if len(maze) - 1 > y and (x, y+1) not in prev:
-        if (x, y + 1) in cache.keys():
-            min_risk = min(min_risk, cache[(x, y+1)] + risk)
-        else:
-            risk_down: int = traverse1(maze, x, y+1, 0, cache, prev.copy())
-            min_risk = min(min_risk, risk_down + risk)
-
-    if x > 0 and (x-1, y) not in prev:
-        if (x - 1, y) in cache.keys():
-            min_risk = min(min_risk, cache[(x-1, y)] + risk)
-        else:
-            risk_left: int = traverse1(maze, x-1, y, 0, cache, prev.copy())
-            min_risk = min(min_risk, risk_left + risk)
-
-    if y > 0 and (x, y-1) not in prev:
-        if (x, y-1) in cache.keys():
-            min_risk = min(min_risk, cache[(x, y-1)] + risk)
-        else:
-            risk_up: int = traverse1(maze, x, y-1, 0, cache, prev.copy())
-            min_risk = min(min_risk, risk_up + risk)
-
-    if (x, y) not in cache.keys() or min(min_risk, cache[(x, y)]) == min_risk: 
-        if x == 3 and y == 1:
-            print(min_risk, risk_right, prev)
-        cache[(x, y)] = min_risk
-
-    # print(cache)
-    return min_risk
-
-if __name__ == "__main__":
-    print(part1())
+import time
+from heapq import heapify, heappop, heappush
+ 
+start_time = time.time()
+ 
+TILES = 5
+REMOVED = -1
+ 
+with open('input/15.txt') as f:
+    map = [[int(ch) for ch in list(row.strip())] for row in f.readlines()]
+ 
+def find_lowest_risk_path(map):
+    height = len(map)
+    width = len(map[0])
+    target = (height - 1, width - 1)
+ 
+    frontier = [(map[0][1], 0, 1), (map[1][0], 1, 0)]
+    heapify(frontier)
+ 
+    def get_neighbours(x, y):
+        """Returns a set of the valid coordinate pairs of all adjacent cells
+        to the given coordinates"""
+        neighbours = set()
+        if x > 0:
+            neighbours.add((x-1, y))
+        if x < height - 1:
+            neighbours.add((x+1, y))
+        if y > 0:
+            neighbours.add((x, y-1))
+        if y < width - 1:
+            neighbours.add((x, y+1))
+        return neighbours
+ 
+ 
+    def find_in_frontier(x, y):
+        """Returns a tuple of the index and current priority (risk) of the 
+        given coordinate pair in the frontier, or None if the given coordinate
+        pair is not in the frontier"""
+        for i in range(len(frontier)):
+            fr_p, fr_x, fr_y = frontier[i]
+            if fr_x == x and fr_y == y:
+                return (i, fr_p)
+        return None
+ 
+   
+    visited = {(0,0)}
+ 
+    while True:
+        while True:
+            # find next (non-removed) cell to visit in the frontier
+            cur_risk, x, y = heappop(frontier)
+            if x != REMOVED:
+                break
+ 
+        if (x, y) == target:
+            return cur_risk
+        visited.add((x, y))
+ 
+        neighbours = get_neighbours(x, y) - visited
+        for n_x, n_y in neighbours:
+            new_risk = cur_risk + map[n_x][n_y]
+            if (fif := find_in_frontier(n_x, n_y)) is not None:
+                # this neighbour is already in the frontier, update it if req'd
+                n_i, n_r = fif
+                if new_risk < n_r:
+                    frontier[n_i] = (n_r, REMOVED, REMOVED)
+                    heappush(frontier, (new_risk, n_x, n_y))
+            else:
+                # this neighbour is new, add it to the frontier
+                heappush(frontier, (new_risk, n_x, n_y))
+ 
+print('Part 1:', find_lowest_risk_path(map), 
+        f'({time.time() - start_time:0.2f} s)')
+start_time = time.time()
+ 
+# Tile the map for part 2
+def inc(row):
+    return [(val + 1 if val < 9 else 1) for val in row]
+ 
+for row in map:
+    iter_row = row.copy()
+    for _ in range(TILES - 1):
+        iter_row = inc(iter_row)
+        row.extend(iter_row)
+new_rows = map.copy()
+for _ in range(TILES - 1):
+    new_rows = [inc(row) for row in new_rows]
+    map.extend(new_rows)
+ 
+print('Part 2:', find_lowest_risk_path(map), 
+        f'({time.time() - start_time:0.2f} s)')
